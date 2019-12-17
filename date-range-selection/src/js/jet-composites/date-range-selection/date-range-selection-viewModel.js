bfbe185c62d0de6ecbf6f49c9f021d9abcc66274
hroknizadeh <h.roknizadeh@it-p.de>
2019-12-17 18:19:25 +0100

    create From and To Range

--- date-range-selection/src/js/jet-composites/date-range-selection/date-range-selection-viewModel.js
+++ date-range-selection/src/js/jet-composites/date-range-selection/date-range-selection-viewModel.js
@@ -1,34 +1,44 @@
 /**
+ * @author Hamed Roknizadeh
+ * @email hamedoracle@gmail.com
+ * @create date 2019-12-17 18:18:19
+ * @modify date 2019-12-17 18:18:19
+ */
+/**
   Copyright (c) 2015, 2019, Oracle and/or its affiliates.
   The Universal Permissive License (UPL), Version 1.0
 */
+
 'use strict';
 define(
-    ['knockout', 'ojL10n!./resources/nls/date-range-selection-strings', 'ojs/ojcontext', 'ojs/ojknockout'], function (ko, componentStrings, Context) {
-    
+  ['knockout', 'ojL10n!./resources/nls/date-range-selection-strings', 'ojs/ojcontext', 'ojs/ojknockout','ojs/ojdatetimepicker'],
+  function (ko, componentStrings, Context) {
+
     function ExampleComponentModel(context) {
-        var self = this;
-        
-        //At the start of your viewModel constructor
-        var busyContext = Context.getContext(context.element).getBusyContext();
-        var options = {"description": "Web Component Startup - Waiting for data"};
-        self.busyResolve = busyContext.addBusyState(options);
+      var self = this;
 
-        self.composite = context.element;
+      //At the start of your viewModel constructor
+      var busyContext = Context.getContext(context.element).getBusyContext();
+      var options = { "description": "Web Component Startup - Waiting for data" };
+      self.busyResolve = busyContext.addBusyState(options);
 
-        //Example observable
-        self.messageText = ko.observable('Hello from Example Component');
-        self.properties = context.properties;
-        self.res = componentStrings['date-range-selection'];
-        // Example for parsing context properties
-        // if (context.properties.name) {
-        //     parse the context properties here
-        // }
+      self.composite = context.element;
 
-        //Once all startup and async activities have finished, relocate if there are any async activities
-        self.busyResolve();
+      //Example observable
+      self.messageText = ko.observable('Hello from Example Component');
+      self.from = ko.observable();
+      self.to = ko.observable();
+      self.properties = context.properties;
+      self.res = componentStrings['date-range-selection'];
+      // Example for parsing context properties
+      // if (context.properties.name) {
+      //     parse the context properties here
+      // }
+
+      //Once all startup and async activities have finished, relocate if there are any async activities
+      self.busyResolve();
     };
-    
+
     //Lifecycle methods - uncomment and implement if necessary 
     //ExampleComponentModel.prototype.activated = function(context){
     //};
@@ -46,4 +56,4 @@ define(
     //};
 
     return ExampleComponentModel;
-});
+  });
--- date-range-selection/src/js/main.js
+++ date-range-selection/src/js/main.js
@@ -10,9 +10,9 @@
  * Example of Require.js boostrap javascript
  */
 
- // The UserAgent is used to detect IE11. Only IE11 requires ES5.
+// The UserAgent is used to detect IE11. Only IE11 requires ES5.
 (function () {
-  
+
   function _ojIsIE11() {
     var nAgt = navigator.userAgent;
     return nAgt.indexOf('MSIE') !== -1 || !!nAgt.match(/Trident.*rv:11./);
@@ -55,24 +55,34 @@
  * by some modules), we are listing it explicitly to get the reference to the 'ko'
  * object in the callback
  */
-require(['ojs/ojbootstrap', 'knockout', 'appController', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojtoolbar', 'ojs/ojmenu'],
+require(['ojs/ojbootstrap'
+  , 'knockout'
+  , 'appController'
+  , 'ojs/ojknockout'
+  , 'ojs/ojbutton'
+  , 'ojs/ojtoolbar'
+  , 'ojs/ojmenu'
+  , 'jet-composites/date-range-selection/1.0.0/loader'
+  ],
   function (Bootstrap, ko, app) { // this callback gets executed when all required modules are loaded
 
-      Bootstrap.whenDocumentReady().then(
-        function() {
-          function init() {
-            // Bind your ViewModel for the content of the whole page body.
-            ko.applyBindings(app, document.getElementById('globalBody'));
-          }
-
-          // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
-          // event before executing any code that might interact with Cordova APIs or plugins.
-          if (document.body.classList.contains('oj-hybrid')) {
-            document.addEventListener("deviceready", init);
-          } else {
-            init();
-          }
+    Bootstrap.whenDocumentReady().then(
+      function () {
+        function init() {
+          // Bind your ViewModel for the content of the whole page body.
+          self.from = ko.observable();
+          self.to = ko.observable();
+          ko.applyBindings(app, document.getElementById('globalBody'));
         }
-      );
-    }
+
+        // If running in a hybrid (e.g. Cordova) environment, we need to wait for the deviceready
+        // event before executing any code that might interact with Cordova APIs or plugins.
+        if (document.body.classList.contains('oj-hybrid')) {
+          document.addEventListener("deviceready", init);
+        } else {
+          init();
+        }
+      }
+    );
+  }
 );
